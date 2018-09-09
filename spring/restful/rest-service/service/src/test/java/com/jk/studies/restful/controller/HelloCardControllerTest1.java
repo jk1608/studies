f35f5f7655888d621f63976a434cd4bbbcb47e2e
kondurj <jayasekhar.konduru@cdk.com>
2018-09-09 14:32:44 +0530

    test cases with more matchers

--- spring/restful/rest-service/service/src/test/java/com/jk/studies/restful/controller/HelloCardControllerTest1.java
+++ spring/restful/rest-service/service/src/test/java/com/jk/studies/restful/controller/HelloCardControllerTest1.java
@@ -1,4 +1,39 @@
 package com.jk.studies.restful.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloCardControllerTest1 {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void noParamForHello() throws Exception {
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/hello"));
+        resultActions = resultActions.andDo(MockMvcResultHandlers.print());
+        StatusResultMatchers statusResultMatchers = MockMvcResultMatchers.status();
+        ContentResultMatchers contentResultMatchers = MockMvcResultMatchers.content();
+        resultActions.andExpect(statusResultMatchers.isOk());
+        ResultMatcher stringResultMatcher = contentResultMatchers.string("{\"id\":1,\"content\":\"Hello, World!\"}");
+        resultActions.andExpect(stringResultMatcher);
+        JsonPathResultMatchers jsonPathResultMatchers = MockMvcResultMatchers.jsonPath("$.content");
+        String expectedContent = "Hello, World!";
+        resultActions.andExpect(jsonPathResultMatchers.value(expectedContent));
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.id").value("1"));
+    }
+
+
 }
